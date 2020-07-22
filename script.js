ebba2958fe245f31e0d8e06a3f59a1630013d04a
Carl Payne <CulinarytrackerDev@Gmail.com>
2020-07-22 17:47:25 -0400

    separate playRound function from game. Impore score and winner notification

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function computerPlay() {
 
 let capFirst = (input) => (input.charAt(0).toUpperCase() + input.substr(1).toLowerCase());
 
-let checkPlayerSelection = (a) => ((a=="rock")||(a=="paper")||(a=="scissors"));
+let checkPlayerSelection = (x) => ((x=="rock")||(x=="paper")||(x=="scissors"));
 
 function getPlayerSelection() {
     let playerSelection = prompt('Choose "Rock", "Paper" or "Scissors":').trim();
@@ -34,27 +34,55 @@ function checkPlayerWin(computerSelection, playerSelection) {
     }
 }
 
-function game(a) {
+function playRound() {
+    player = getPlayerSelection();
+    computer = computerPlay();
+    if (checkForTie(computer, player)) {
+        console.log(`\nTie Game! Both you and the computer chose ${player}.`);
+        return 1;
+    }else if (checkPlayerWin(computer, player)) {
+        console.log(`\n${player} beats ${computer}! You Win!`);
+        return 2;
+    }else {
+        console.log(`\n${computer} beats ${player}! Computer Wins!`);
+        return 3;
+    }
+}
+
+function sanitizeNumber(n) {
+    if (!isNaN(n.trim()) && n.trim()%1==0) {
+        return n;
+    }else {
+        return sanitizeNumber(prompt("That's not a valid number. How many rounds do you want to play?"));
+    }
+}
+
+function game(numOfRounds) {
     let score = [0, 0];
     let gamesPlayed = 0;
     let player;
     let computer;
-
-    for (let i=0; i<a; i++){
-        player = getPlayerSelection();
-        computer = computerPlay();
+    let currentRound;
+    for (let i=0; i<numOfRounds; i++){
         gamesPlayed++;
-        if (checkForTie(computer, player)) {
-            console.log(`Tie Game! Both you and the computer chose ${player}.\nCurrent Score after ${gamesPlayed} games:\nPlayer:  ${score[0]}\nComputer: ${score[1]}`);
-        }else if (checkPlayerWin(computer, player)) {
+        currentRound=playRound();
+        if (currentRound==1) {
+            console.log(`\nScore after ${gamesPlayed} games:\nYou:  ${score[0]}\nComputer: ${score[1]}`);
+        }else if (currentRound==2) {
             score[0]++;
-            console.log(`${player} beats ${computer}! Player Wins!\nCurrent Score after ${gamesPlayed} games:\nPlayer:  ${score[0]}\nComputer: ${score[1]}`)
+            console.log(`\nScore after ${gamesPlayed} games:\nYou:  ${score[0]}\nComputer: ${score[1]}`);
         }else {
             score[1]++;
-            console.log(`${computer} beats ${player}! Computer Wins!\nCurrent Score after ${gamesPlayed} games:\nPlayer:  ${score[0]}\nComputer: ${score[1]}`)
+            console.log(`\nScore after ${gamesPlayed} games:\nYou:  ${score[0]}\nComputer: ${score[1]}`);
         }
-    
+    }
+    if (score[0]>score[1]) {
+        console.log(`\nCongratulations to You! After ${gamesPlayed} rounds you beat the computer ${score[0]} to ${score[1]}.`);
+    }else if (score[0]<score[1]) {
+        console.log(`\nComputer Wins! After ${gamesPlayed} rounds you were beat ${score[0]} to ${score[1]}.`);
+    }else {
+        console.log(`\nIt ends in a tie! After ${gamesPlayed} rounds it was tied at ${score[0]}.`);
     }
 }
 
-game(5);
+game(sanitizeNumber(prompt('How many rounds would you like to play?')));
